36ba5545c3bb38d0459e64628862cfcd113fc73f
Jorj Pimm <jorjpimm@gmail.com>
2013-02-21 17:05:00 +0000

    initial work on a unit test

--- ShiftCoreTest/computetest.cpp
+++ ShiftCoreTest/computetest.cpp
@@ -1,5 +1,37 @@
 #include <QString>
 #include <QtTest>
+#include "shift/sentity.h"
+#include "shift/sdatabase.h"
+#include "shift/Properties/sbaseproperties.h"
+
+class TestVector : public Shift::PropertyContainer
+  {
+  S_ENTITY(TestEntity, Entity, 0)
+public:
+
+  Shift::FloatProperty x;
+  Shift::FloatProperty y;
+  Shift::FloatProperty z;
+
+  bool allDirty() const
+    {
+    return isDirty() && x.isDirty() && y.isDirty() && z.isDirty();
+    }
+
+  bool anyDirty() const
+    {
+    return !(isDirty() | x.isDirty() || y.isDirty() || z.isDirty());
+    }
+  };
+
+class TestEntity : public Shift::Entity
+  {
+  S_ENTITY(TestEntity, Entity, 0)
+public:
+
+  TestVector in;
+  TestVector reciprocal;
+  };
 
 class ShiftCoreComputeTest : public QObject
   {
@@ -10,6 +42,9 @@ public:
 
 private Q_SLOTS:
   void testCase1();
+
+private:
+  Shift::Database _db;
   };
 
 ShiftCoreComputeTest::ShiftCoreComputeTest()
@@ -18,10 +53,17 @@ ShiftCoreComputeTest::ShiftCoreComputeTest()
 
 void ShiftCoreComputeTest::testCase1()
   {
-  QVERIFY2(true, "Failure");
+  TestEntity* a = _db.addChild<TestEntity>();
+  QCOMPARE(a->allDirty(), true);
 
-  QBENCHMARK {
-    }
+  a->preGet();
+  QCOMPARE(a->anyDirty(), false);
+
+  b->in.setInput(a->reciprocal);
+  TestEntity* b = _db.addChild<TestEntity>();
+
+  c->in.setInput(b->reciprocal);
+  TestEntity* c = _db.addChild<TestEntity>();
   }
 
 QTEST_APPLESS_MAIN(ShiftCoreComputeTest)
