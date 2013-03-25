e380b17d8f20256003c9d20115d3f7ff8dc9031c
Jorj Pimm <jorjpimm@gmail.com>
2013-03-25 16:42:25 +0000

    work on a node debugger

--- ShiftCore/include/shift/UI/sdebugger.h
+++ ShiftCore/include/shift/UI/sdebugger.h
@@ -1,12 +1,48 @@
 #ifndef SDEBUGGER_H
 #define SDEBUGGER_H
 
+#include "shift/sglobal.h"
 #include "QtWidgets/QWidget"
+#include "QtWidgets/QGraphicsItem"
+#include "QtGui/QStaticText"
 
-class Debugger : public QWidget
+class QGraphicsScene;
+
+namespace Shift
+{
+
+class Database;
+
+class SHIFT_EXPORT Debugger : public QWidget
+  {
+  Q_OBJECT
+
+public:
+  Debugger(Database *db, QWidget *parent=0);
+
+private slots:
+  void snapshot();
+
+private:
+  QGraphicsItem *createItemForProperty(Property *prop);
+
+  QGraphicsScene *_scene;
+  Database *_db;
+  };
+
+class DebugPropertyItem : public QGraphicsItem
   {
 public:
-  Debugger(QObject *parent=0);
+  DebugPropertyItem(const QString &text);
+
+  QRectF boundingRect() const;
+
+  void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+private:
+  QStaticText _info;
   };
+
+}
 
 #endif // SDEBUGGER_H
--- ShiftCore/src/UI/sdebugger.cpp
+++ ShiftCore/src/UI/sdebugger.cpp
@@ -1,5 +1,97 @@
 #include "shift\UI\sdebugger.h"
+#include "QtWidgets/QToolBar"
+#include "QtWidgets/QVBoxLayout"
+#include "QtWidgets/QGraphicsView"
+#include "QtWidgets/QGraphicsScene"
+#include "shift/sdatabase.h"
+#include "shift/Properties/sproperty.h"
+#include "shift/Properties/spropertycontaineriterators.h"
+#include "shift/TypeInformation/sinterfaces.h"
 
-Debugger::Debugger(QObject *parent)
+namespace Shift
+{
+
+Debugger::Debugger(Shift::Database *db, QWidget *parent) : QWidget(parent)
   {
+  _db = db;
+
+  move(10, 10);
+
+  QVBoxLayout *layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0, 0, 0, 0);
+  setLayout(layout);
+
+  QToolBar *toolbar = new QToolBar("mainTool", this);
+  layout->addWidget(toolbar);
+
+  toolbar->addAction("Snapshot", this, SLOT(snapshot()));
+
+  _scene = new QGraphicsScene(this);
+
+  _scene->setBackgroundBrush(QColor(64, 64, 64));
+
+  QGraphicsView *view = new QGraphicsView(_scene, this);
+  layout->addWidget(view);
   }
+
+void Debugger::snapshot()
+  {
+  QGraphicsItem *snapshot = createItemForProperty(_db);
+
+  _scene->addItem(snapshot);
+  }
+
+QGraphicsItem *Debugger::createItemForProperty(Property *prop)
+  {
+  QString text = prop->name().toQString();
+  PropertyVariantInterface *ifc = prop->interface<PropertyVariantInterface>();
+  if(ifc)
+    {
+    text + "\nvalue: " + ifc->asString(prop).toQString();
+    }
+
+  enum
+    {
+    ChildOffsetX = 100,
+    ChildOffsetY = 50
+    };
+
+  DebugPropertyItem *item = new DebugPropertyItem(text);
+
+  PropertyContainer *c = prop->castTo<PropertyContainer>();
+  if(c)
+    {
+    xsize index = 0;
+    xForeach(auto p, c->walker())
+      {
+      QGraphicsItem *childItem = createItemForProperty(p);
+      childItem->setPos(index * ChildOffsetX, ChildOffsetY);
+      childItem->setParentItem(item);
+      }
+    }
+
+  item->setPos(width()/2, height()/2);
+  return item;
+  }
+
+DebugPropertyItem::DebugPropertyItem(const QString &text)
+    : _info(text)
+  {
+  setAcceptedMouseButtons(Qt::LeftButton)
+  }
+
+QRectF DebugPropertyItem::boundingRect() const
+  {
+  qreal penWidth = 1;
+  return QRectF(-10 - penWidth / 2, -10 - penWidth / 2,
+                20 + penWidth, 20 + penWidth);
+  }
+
+void DebugPropertyItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
+  {
+  painter->setPen(QPen(Qt::red));
+  painter->setBrush(Qt::blue);
+  painter->drawRoundedRect(-10, -10, 20, 20, 5, 5);
+  }
+
+}
